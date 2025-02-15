1f3b5e6431a352c6a2521044f1217e5795dbcf6a
Dewmal <dewmal@syigen.com>
2025-02-15 09:07:24 +0530

    export

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    export: true,
+    output: "export",
+    images: {unoptimized: true}
 };
 
 export default nextConfig;
